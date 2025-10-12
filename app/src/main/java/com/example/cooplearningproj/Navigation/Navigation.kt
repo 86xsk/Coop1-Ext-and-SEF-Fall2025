7ca0877ce22d65a407812888e60453e961973bf0
86xsk <200443667+86xsk@users.noreply.github.com>
2025-10-12 12:41:52 -0500

    init

---
+++ app/src/main/java/com/example/cooplearningproj/Navigation/Navigation.kt
@@ -0,0 +1,14 @@
+package com.example.cooplearningproj.Navigation
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.wear.compose.navigation.currentBackStackEntryAsState
+
+@Composable
+fun BottomNav(navController: NavController) {
+    NavigationBar {
+        val navBackStackEntry = navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry.value?.destination
+    }
+}
