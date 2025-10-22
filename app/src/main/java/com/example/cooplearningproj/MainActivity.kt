d144aae9649cede9a3db74575de24519caf2cb67
86xsk <200443667+86xsk@users.noreply.github.com>
2025-10-22 11:19:45 -0500

    feat: add `TopAppBar`

--- app/src/main/java/com/example/cooplearningproj/MainActivity.kt
+++ app/src/main/java/com/example/cooplearningproj/MainActivity.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.cooplearningproj.destination.Destination
+import com.example.cooplearningproj.extensions.color.darken
+import com.example.cooplearningproj.extensions.color.lighten
 import com.example.cooplearningproj.navigation.BottomNav
 import com.example.cooplearningproj.navigation.NAV_ITEMS
 import com.example.cooplearningproj.screens.EditScreen
@@ -32,6 +37,20 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("Temp placeholder")
+                            },
+                            colors = TopAppBarColors(
+                                containerColor = Color.Green.darken(0.3f),
+                                scrolledContainerColor = Color.Green,
+                                navigationIconContentColor = Color.White,
+                                titleContentColor = Color.White,
+                                actionIconContentColor = Color.Red
+                            )
+                        )
+                    },
                     bottomBar = {
                         BottomNav(navController)
                     }
