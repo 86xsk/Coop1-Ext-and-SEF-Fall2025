3999a4066e26f066026258ddd0bc712f32643f43
86xsk <200443667+86xsk@users.noreply.github.com>
2025-10-22 11:19:26 -0500

    feat: add `ColorExt`

---
+++ app/src/main/java/com/example/cooplearningproj/extensions/color/ColorExt.kt
@@ -0,0 +1,10 @@
+package com.example.cooplearningproj.extensions.color
+
+import androidx.annotation.FloatRange
+import androidx.compose.ui.graphics.Color
+
+fun Color.darken(@FloatRange(0.0, 1.0) amount: Float) =
+    this.copy(red = red * (1f - amount), green = green * (1f - amount), blue = blue * (1f - amount))
+
+fun Color.lighten(@FloatRange(0.0, 1.0, fromInclusive = false) amount: Float) =
+    this.copy(red = red / (1f - amount), green = green / (1f - amount), blue = blue / (1f - amount))
